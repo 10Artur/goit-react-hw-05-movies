a0907ee7b9be3167c02207f33d3acce2fe72a359
10Artur <artyr11012002@gmail.com>
2023-09-07 23:20:39 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import { GlobalStyle } from './GlobalStyle';
+import { Routes, Route } from 'react-router-dom';
+// export const App = () => {
+//   return (
+//     <div>
+//       React homework template
+//       <GlobalStyle />
+//     </div>
+//   );
+// };
 
 export const App = () => {
-  return (
-    <div>
-      React homework template
-      <GlobalStyle />
-    </div>
-  );
+  return <Routes></Routes>;
 };
